682ad796dd3fad4b76f803b1e2572801fec8ed05
Charlie Peppler <cpeppler@rocelec.com>
2020-10-27 11:09:37 -0400

    Add project files.

---
+++ CamstarExtractService/CamstarExtractService.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using ExtractQueryCore;
+using NLog;
+
+
+namespace CamstarExtractService
+{
+    public partial class CamstarExtractService : ServiceBase
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        Timer timer = new Timer(); // name space(using System.Timers;)
+        private CamstarExtractor csx;
+        public CamstarExtractService()
+        {
+            InitializeComponent();
+            this.ServiceName = "CamstarExtractService";
+        }
+        protected override void OnStart(string[] args)
+        {
+
+            timer.Interval = 500; //number in milisecinds  
+            logger.Info(String.Format("Service {0} has been started at {1}, Timer set to {2}", this.ServiceName, DateTime.Now, timer.Interval));               // Manual Log
+            try
+            {
+                logger.Info(String.Format("Running CamstarExtractor"));               // Manual Log
+                csx = new CamstarExtractor();
+                logger.Info(String.Format("CamstarExtractor object created successfully."));               // Manual Log
+            }
+            catch (Exception e)
+            {
+                logger.Fatal(String.Format("Service {0} has thrown an exception! {1} at {2}. Shuttting down.", this.ServiceName, e.Message, DateTime.Now));               // Manual Log
+                ServiceController svc = new ServiceController("CamstarExtractService");
+                svc.Stop();
+            }
+            logger.Info("Service is started at " + DateTime.Now);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Enabled = true;
+        }
+        protected override void OnStop()
+        {
+            logger.Info(String.Format("Service {0} is stopping at {1}", this.ServiceName, DateTime.Now));               // Manual Log
+            csx = null;
+            logger.Info("Service is stopped at " + DateTime.Now);
+        }
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            logger.Info("Service {0} is recalled at {1}.", this.ServiceName, DateTime.Now); // Manual Log
+            timer.Stop();
+            timer.Enabled = false;
+            TimeSpan ts = csx.RunUntilDone();
+            timer.Interval = ts.TotalSeconds * 1000;
+            timer.Enabled = true;
+            timer.Start();
+
+        }
+#if false
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to.   
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
+#endif
+    }
+}
---
+++ CamstarExtractService/ProjectInstaller.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace CamstarExtractService
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;   // Added per https://www.c-sharpcorner.com/article/create-windows-services-in-c-sharp/
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            // 
+            // serviceInstaller1
+            // 
+            this.serviceInstaller1.Description = "Camstar Extract Service";
+            this.serviceInstaller1.DisplayName = "CamstarExtractService"; 
+            this.serviceInstaller1.ServiceName = "CamstarExtractService";
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.serviceInstaller1});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller1;
+    }
+}
